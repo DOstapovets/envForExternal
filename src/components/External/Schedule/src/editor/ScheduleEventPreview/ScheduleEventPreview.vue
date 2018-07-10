47ba0b38909eec41faecc74dadf067f5e2423200
Dmytro Lisovskyi <lisovskyi.dmytro@gmail.com>
2018-07-10 13:41:01 +0300

    validation fix26

--- src/components/External/Schedule/src/editor/ScheduleEventPreview/ScheduleEventPreview.vue
+++ src/components/External/Schedule/src/editor/ScheduleEventPreview/ScheduleEventPreview.vue
@@ -1,6 +1,5 @@
 <template>
   <div :class="['schedule-event-preview', {'schedule-event-preview_invalid': false}]" @click="doEditable">
-    {{startsAt}}
     <template v-if="!false">
       <span 
           class="schedule-event-preview__circle"
