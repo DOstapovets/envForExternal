f2243a091a430da448cb6f935be80a7aa726c67d
Dmytro Lisovskyi <lisovskyi.dmytro@gmail.com>
2018-07-10 07:30:11 +0300

    validation fix26

--- src/components/External/Schedule/src/editor/ScheduleEventPreview/ScheduleEventPreview.vue
+++ src/components/External/Schedule/src/editor/ScheduleEventPreview/ScheduleEventPreview.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="['schedule-event-preview', {'schedule-event-preview_invalid': false}]" @click="doEditable">
+  <!-- <div :class="['schedule-event-preview', {'schedule-event-preview_invalid': false}]" @click="doEditable"> -->
     <!-- <template v-if="!false">
       <span 
           class="schedule-event-preview__circle"
@@ -65,7 +65,8 @@
         >
         </or-menu>
     </or-icon-button> -->
-  </div>
+  <!-- </div> -->
+  <div>sdfds</div>
 </template>
 
 <script>
