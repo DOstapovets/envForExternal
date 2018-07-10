aaa5385bdb830dbcc753aa8cff1f55f395322b55
Dmytro Lisovskyi <lisovskyi.dmytro@gmail.com>
2018-07-10 14:10:39 +0300

    validation fix26

--- src/components/External/Schedule/src/editor/ScheduleEventPreview/ScheduleEventPreview.vue
+++ src/components/External/Schedule/src/editor/ScheduleEventPreview/ScheduleEventPreview.vue
@@ -11,8 +11,10 @@
             <span
               :key="indexOfStartsAt"
               v-for="(date, indexOfStartsAt) in startsAt || []"
-              :class="{'bold-text': !!(indexOfStartsAt === 0)}"
-              v-show="!!((indexOfStartsAt < countAtDates) && (indexOfStartsAt < 3 || moreDates))"
+              :class="{
+                'bold-text': !!(indexOfStartsAt === 0), 
+                'hide': !!((indexOfStartsAt < countAtDates) && (indexOfStartsAt < 3 || moreDates))
+              }"
             >
             <!-- v-if="!!((indexOfStartsAt < countAtDates) && (indexOfStartsAt < 3 || moreDates))" -->
             <!-- {{date}}<span v-if="startsAt && indexOfStartsAt !== startsAt.length - 1">,</span><span v-if="indexOfStartsAt >= countAtDates - 1 || (startsAt && indexOfStartsAt === 2 && !moreDates && startsAt.length > 3)">...</span> -->
