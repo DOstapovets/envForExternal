a7ef4e451ab375808fbffb456427eb1f75ca3a4b
Dmytro Lisovskyi <lisovskyi.dmytro@gmail.com>
2018-07-10 15:03:11 +0300

    validation fix26

--- src/components/External/Schedule/src/editor/ScheduleEventPreview/ScheduleEventPreview.vue
+++ src/components/External/Schedule/src/editor/ScheduleEventPreview/ScheduleEventPreview.vue
@@ -14,7 +14,7 @@
               :class="{
                 'bold-text': !!(indexOfStartsAt === 0)
               }"
-              v-if="!!(4 < countAtDates) && (4 < 3) || moreDates))"
+              v-if="!!((true < countAtDates) && ((true < 3) || moreDates))"
             >
             <!-- v-if="!!((indexOfStartsAt < countAtDates) && (indexOfStartsAt < 3 || moreDates))" -->
             <!-- {{date}}<span v-if="startsAt && indexOfStartsAt !== startsAt.length - 1">,</span><span v-if="indexOfStartsAt >= countAtDates - 1 || (startsAt && indexOfStartsAt === 2 && !moreDates && startsAt.length > 3)">...</span> -->
