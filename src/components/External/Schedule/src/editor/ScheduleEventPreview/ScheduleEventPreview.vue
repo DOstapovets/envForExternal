06d9e263a010f8fec820ee83a33650ada5a60775
Dmytro Lisovskyi <lisovskyi.dmytro@gmail.com>
2018-07-10 08:01:50 +0300

    validation fix26

--- src/components/External/Schedule/src/editor/ScheduleEventPreview/ScheduleEventPreview.vue
+++ src/components/External/Schedule/src/editor/ScheduleEventPreview/ScheduleEventPreview.vue
@@ -7,7 +7,7 @@
         ></span>
         <div class="schedule-event-preview__content">
           <div class="schedule-event-preview__title-text">{{eventName}}</div>
-          <div class="schedule-event-preview__dates">
+          <!-- <div class="schedule-event-preview__dates">
             <span
               :key="index"
               v-for="(date, index) in startsAt"
@@ -23,8 +23,8 @@
             >
               see more
             </span>
-          </div>
-          <!-- <div class="schedule-event-preview__times">
+          </div> -->
+          <div class="schedule-event-preview__times">
             <span 
               :key="time.id" 
               v-for="(time, index) in startTimes"
@@ -38,7 +38,7 @@
               >
                 see more
               </span>
-          </div> -->
+          </div>
           <!-- <div
             class="schedule-event-preview__end-date"
             v-html="endDateComp">
